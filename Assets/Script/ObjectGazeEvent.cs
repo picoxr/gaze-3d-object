3cdc42162498e779a694bf20b7b27eabf0c913fc
俊伟 娄 <958868128@qq.com>
2019-01-04 14:55:25 +0800

    Update
    Update

---
+++ Assets/Script/ObjectGazeEvent.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectGazeEvent : MonoBehaviour//, IPointerClickHandler, IPointerEnterHandler
+{
+    private EventSystem m_CurEvent;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_CurEvent = EventSystem.current;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    public void OnPointerClick(/*PointerEventData data*/)
+    {
+        m_CurEvent.SetSelectedGameObject(gameObject);
+        Debug.Log("OnPointerClick");
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+
+    public void OnPointerEnter(/*PointerEventData data*/)
+    {
+        Debug.Log("OnPointerEnter");
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    
+}
